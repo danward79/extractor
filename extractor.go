b7f87168320da58ea4892822558d771ffb5b7924
Dan <danward@Daniels-MacBook-Pro.local>
2016-03-04 07:40:55 +1100

    reworking it

--- extractor.go
+++ extractor.go
@@ -14,33 +14,41 @@ import (
 //ParseTarReader loops through a tar.Reader and creates the file or folder.
 func ParseTarReader(tarReader *tar.Reader, destination string) error {
 
+	fmt.Println("ParseTarReader")
+
 	for {
+		fmt.Println("For")
 		hdr, err := tarReader.Next()
+		fmt.Println(hdr)
 		if err == io.EOF {
+			fmt.Println("io.EOF")
 			break
 		} else if err != nil {
 			return fmt.Errorf("Tar Header Error: %v", err)
 		}
 
+		fmt.Println(hdr.Name)
 		path := filepath.Join(destination, hdr.Name)
 		info := hdr.FileInfo()
 
 		switch {
+
 		case info.IsDir():
+			fmt.Println("IsDir")
 			if err = os.MkdirAll(path, info.Mode()); err != nil {
 				return fmt.Errorf("Error generating folder: %v", err)
 			}
 			continue
 
 		case strings.HasSuffix(info.Name(), ".gz") && !strings.HasPrefix(info.Name(), "."):
-
+			fmt.Println(".gz")
 			err = gzDecompress(tarReader, hdr, path)
 			if err != nil {
 				return err
 			}
 
 		case strings.HasSuffix(info.Name(), ".tar"):
-
+			fmt.Println(".tar")
 			err := copyReaderFile(tarReader, hdr, path)
 			if err != nil {
 				return err
